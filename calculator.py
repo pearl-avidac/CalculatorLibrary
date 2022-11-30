77edd34d54fe799a7978bfa47088f61288384794
Pearl Sailikebuli <psailikebuli@avidac.com>
2022-11-30 14:40:56 -0700

    added multiply function

--- calculator.py
+++ calculator.py
@@ -9,3 +9,6 @@ def add(first_term, second_term):
 
 def substract(first_term, second_term):
     return first_term-second_term
+
+def multiply(first_name, second_name):
+    return first_name*second_name
