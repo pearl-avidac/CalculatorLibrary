aff56b3d46733d6e2d7385a30b877160bcd4859a
Pearl Sailikebuli <psailikebuli@avidac.com>
2022-11-30 12:50:43 -0700

    edited test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,11 @@ Unit tests for the calculator library
 
 import calculator
 
+
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
-        assert 2 == calculator.substract(4,2)
+        assert 2 == calculator.substract(4, 2)
