3514b8a5ae51dffdd0ca1e171440471e51b1b6f3
Pearl Sailikebuli <psailikebuli@avidac.com>
2022-11-30 14:42:58 -0700

    style change

--- calculator.py
+++ calculator.py
@@ -10,5 +10,6 @@ def add(first_term, second_term):
 def substract(first_term, second_term):
     return first_term-second_term
 
-def multiply(first_name, second_name):
-    return first_name*second_name
+
+def multiply(first_term, second_term):
+    return first_term*second_term
